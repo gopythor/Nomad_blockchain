0472ccf69aa9f5beaa76fb385faf3786e35ced4d
SoliDeoHoneretGloria <iphone_1st@naver.com>
2022-07-19 20:49:21 +0200

    json

---
+++ 5.2/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"nomadcoin/Nomad_blockchain/5.1/blockchain"
+)
+
+const port string = ":4000"
+
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/home.gohtml"))
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	tmpl.Execute(rw, data)
+}
+
+func main() {
+	http.HandleFunc("/", home)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
---
+++ 5.5/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "nomadcoin/Nomad_blockchain/5.5/explorer"
+
+func main() {
+	explorer.Start()
+}
